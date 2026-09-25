eabb899fd91d78c69878ca8428fa3656c1449402
Shannon Smith <infiltratr@yandex.com>
2026-09-25 15:35:27 +1000

    Add fast installed-state reconciliation

--- src/engine/engine_service_core.hpp
+++ src/engine/engine_service_core.hpp
@@ -33,6 +33,7 @@ public:
         DebianCandidatePolicy policy = {});
 
     bool reload(std::string &error);
+    bool refresh_installed(std::string &error);
     bool refresh(std::string &error);
 
     [[nodiscard]] EngineServiceStatus status() const;
